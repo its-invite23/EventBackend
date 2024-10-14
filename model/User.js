953dcc503bed2067da0edd946c08148e10ad6df4
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-10-14 11:52:07 +0530

    manage setup

--- model/User.js
+++ model/User.js
@@ -1,18 +1,38 @@
-const  mongoose =  require("mongoose");
+const mongoose = require("mongoose");
 
 
 
 const userSchema = mongoose.Schema({
 
-    username :{
-        type :String,
-        required : true,
-        unique : true
+    username: {
+        type: String,
+        required: true,
+        unique: true
     },
 
-    password : {
-        type : String,
-        required : true
+    password: {
+        type: String,
+        required: true
     },
-    
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: Number,
+        required: true
+    }
 })
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
