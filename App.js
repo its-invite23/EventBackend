2f60a02b85b2642d750d890d5c02cae707ef27b0
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-10-22 17:13:17 +0530

    manage App

--- App.js
+++ App.js
@@ -27,6 +27,12 @@ app.use("/package", require('./route/Package'));
 app.use("/booking", require('./route/Booking'));
 app.use("/contact", require('./route/Contact'));
 
+app.get("/", (req, res) => {
+  res.json({
+    msg: 'Okay',
+    status: 200,
+  });
+});
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
