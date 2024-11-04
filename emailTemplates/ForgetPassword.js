44f508523802326ff94a74fbacf2fd5cfe71a566
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-11-04 13:24:45 +0530

    manage

--- emailTemplates/ForgetPassword.js
+++ emailTemplates/ForgetPassword.js
@@ -1,6 +1,6 @@
 module.exports = (resetLink, userName) => {
     return `
-  <table style="max-width: 400px; font-family: arial;" align="center" width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#000">
+  <table style="max-width: 400px; font-family: arial; margin: 0 auto;" align="center" width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#000">
 <tr bgcolor="#141414">
     <td style="padding: 20px 2px 0 2px; text-align: center;">
         <p style="margin: 1px;">
@@ -20,54 +20,54 @@ module.exports = (resetLink, userName) => {
 </tr>
 
 <tr>
-    <td style="padding:40px 0 30px 20px;">
-        <p style="margin: 1px; font-size: 12px; font-weight: normal; color:#CCCCCC">
+    <td style="padding: 40px 0 30px 0; text-align: center;">
+        <p style="margin: 1px; font-size: 12px; font-weight: normal; color: #CCCCCC;">
             Hi ${userName},
         </p>
     </td>
 </tr>
 
 <tr>
-    <td style="padding: 0 0 30px 20px;">
-        <p style="margin: 1px; font-size: 12px; font-weight: normal; color:#CCCCCC; text-align: center;">
+    <td style="padding: 0 0 30px 0; text-align: center;">
+        <p style="margin: 1px; font-size: 12px; font-weight: normal; color: #CCCCCC;">
             We received a request to reset the password for your account. If you didn't make this request, please ignore this email.
         </p>
     </td>
 </tr>
 
 <tr>
-    <td style="padding: 0 0 30px 20px;">
-        <p style="margin: 1px; font-size: 12px; font-weight: normal; color:#CCCCCC; text-align: center;">
+    <td style="padding: 0 0 30px 0; text-align: center;">
+        <p style="margin: 1px; font-size: 12px; font-weight: normal; color: #CCCCCC;">
             To reset your password, click the link below:
         </p>
     </td>
 </tr>
 
 <tr>
-    <td style="padding: 15px 0 50px 20px; text-align: center;">
-        <a href=${resetLink} target="_blank" style="padding:15px 20px; background: #EB3465; font-size: 12px; text-align: center; color: #ffffff; text-decoration: none; border-radius: 60px; -webkit-border-radius: 60px;">Reset My Password</a>
+    <td style="padding: 15px 0 50px 0; text-align: center;">
+        <a href=${resetLink} target="_blank" style="padding: 15px 20px; background: #EB3465; font-size: 12px; color: #ffffff; text-decoration: none; border-radius: 60px; display: inline-block;">Reset My Password</a>
     </td>
 </tr>
 
 <tr>
-    <td style="padding: 0 0 30px 20px;">
-        <p style="margin: 1px; font-size: 12px; font-weight: normal; color:#CCCCCC">
+    <td style="padding: 0 0 30px 0; text-align: center;">
+        <p style="margin: 1px; font-size: 12px; font-weight: normal; color: #CCCCCC;">
             For security purposes, this link will expire in 24 hours. If you need further assistance or didn’t request this reset, please reach out to our support team immediately.
         </p>
     </td>
 </tr>
 
 <tr>
-    <td style="padding: 0 0 30px 20px;">
-        <p style="margin: 1px; font-size: 12px; font-weight: normal; color:#CCCCCC">
+    <td style="padding: 0 0 30px 0; text-align: center;">
+        <p style="margin: 1px; font-size: 12px; font-weight: normal; color: #CCCCCC;">
             Thank you for using Event Management!
         </p>
     </td>
 </tr>
 
 <tr>
-    <td style="padding: 0 0 45px 20px;">
-        <p style="margin: 1px; font-size: 12px; font-weight: normal; color:#CCCCCC">
+    <td style="padding: 0 0 45px 0; text-align: center;">
+        <p style="margin: 1px; font-size: 12px; font-weight: normal; color: #CCCCCC;">
             Best regards, <br> Event Management Support Team
         </p>
     </td>
@@ -86,16 +86,16 @@ module.exports = (resetLink, userName) => {
 </tr>
 
 <tr bgcolor="#141414">
-    <td style="padding: 15px 0 5px 0;border-top:1px solid #444444">
-        <p style="margin: 1px; font-size: 12px; font-weight: normal; color:#CCCCCC; text-align: center;">
+    <td style="padding: 15px 0 5px 0; border-top: 1px solid #444444; text-align: center;">
+        <p style="margin: 1px; font-size: 12px; font-weight: normal; color: #CCCCCC;">
             Copyright (C) 2024 invite. All rights reserved.
         </p>
     </td>
 </tr>
 
 <tr bgcolor="#141414">
-    <td style="padding: 0 0 5px 0;">
-        <p style="margin: 1px; font-size: 12px; font-weight: normal; color:#CCCCCC; text-align: center;">
+    <td style="padding: 0 0 5px 0; text-align: center;">
+        <p style="margin: 1px; font-size: 12px; font-weight: normal; color: #CCCCCC;">
             You are receiving this email because you requested a password reset for your account.
         </p>
     </td>
