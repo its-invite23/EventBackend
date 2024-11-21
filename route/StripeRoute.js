e7dc02281730a90f7f2472ba3be3af86cd7304c0
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-11-21 14:34:49 +0530

    PaymentLink

--- route/StripeRoute.js
+++ route/StripeRoute.js
@@ -1,11 +1,6 @@
 const StripeRoute = require("express").Router();
 const { verifyToken } = require("../controller/AuthController");
 const { createCheckout, PaymentGet } = require("../controller/StripeController");
-
 StripeRoute.post("/create-checkout-session", createCheckout);
-
 StripeRoute.get("/payment-get", PaymentGet);
-
-
-
 module.exports = StripeRoute;
