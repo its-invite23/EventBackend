82c69e53f3fc4a27c015be58338afdd2b4ea22d7
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-12-21 16:33:40 +0530

    mabnah

--- utils/Logger.js
+++ utils/Logger.js
@@ -7,7 +7,7 @@ const logger = winston.createLogger({
     ),
     transports: [
       new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'logs/combinedlog.log' })
+      new winston.transports.File({ filename: 'logs/combinedlog.log' , level: 'info' })
     ]
   });
 
